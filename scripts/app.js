fe23b428df1fe3d719dfb7cb67ee96ab1e7b6016
UnShame <mrunshame@gmail.com>
2019-01-27 02:25:24 +0300

    Set global page to resolved value of promise instead of promise itself

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@ initPage();
 async function initPage() {
     let phones = await fetchJSON('./phones/phones.json');
     phones.forEach(phone => phone.urlBase = location.pathname + 'phones/');
-    window.page = loadAndCreateComponent('page', {
+    window.page = await loadAndCreateComponent('page', {
         element: document.querySelector('[data-page-container]'),
         childrenData: {
             catalogData: phones,
